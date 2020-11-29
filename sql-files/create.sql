1e77c762b379e834edcad0640d79d47a50bd6bc2
Prashant Singh <prashant_singh@live.in>
2020-11-29 01:13:08 -0600

    add sql file for create-schema

---
+++ sql-files/create.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE yawsip;
+
+USE yawsip;
+
+CREATE TABLE user (
+    fname VARCHAR(150) NOT NULL,
+    lname VARCHAR(150) NOT NULL, 
+    username VARCHAR(150) NOT NULL PRIMARY KEY,
+    password VARCHAR(150) NOT NULL,
+    groupnames VARCHAR(150) NOT NULL,
+    status VARCHAR(50) DEFAULT 'Pending'
+);
+
+CREATE TABLE usergroup (
+    name VARCHAR(150) NOT NULL PRIMARY KEY,
+    filename VARCHAR(150) NOT NULL,
+    groupname VARCHAR(150) NOT NULL
+);
