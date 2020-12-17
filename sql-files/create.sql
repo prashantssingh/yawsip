628efa98ad9537ba33a60cf90f0848d7150ededf
Prashant Singh <prashant_singh@live.in>
2020-12-16 20:07:57 -0600

    update create-schema

--- sql-files/create.sql
+++ sql-files/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE user (
     lname VARCHAR(150) NOT NULL,
     email VARCHAR(150) NOT NULL,
     username VARCHAR(150) NOT NULL PRIMARY KEY,
-    password VARCHAR(150) NOT NULL,
+    password VARCHAR(500) NOT NULL,
     role VARCHAR(30) DEFAULT 'User',
     groupnames VARCHAR(200),
     status VARCHAR(50) DEFAULT 'Pending'
@@ -24,43 +24,8 @@ CREATE TABLE upload (
     username VARCHAR(150) NOT NULL,
     filename VARCHAR(150) NOT NULL,
     groupname VARCHAR(150) NOT NULL,
-    createdat DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedat DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO user VALUES ("Prashant", "Singh", "94.prashantsingh@gmail.com", "prashantsingh", "helloworld", "Admin", "Approved");
-INSERT INTO user VALUES ("Pranit", "Jaiswal", "pranitj@gmail.com", "pranitj", "helloworld", "User", "Group 1, Group 2", "Pending");
-INSERT INTO user_group VALUES ("pranitj", "Group 1");
-INSERT INTO user_group VALUES ("pranitj", "Group 2");
-
-DROP DATABASE IF EXISTS yawsip;
-
-CREATE DATABASE yawsip;
-
-USE yawsip;
-
-CREATE TABLE user (
-    fname VARCHAR(150) NOT NULL,
-    lname VARCHAR(150) NOT NULL,
-    email VARCHAR(150) NOT NULL,
-    username VARCHAR(150) NOT NULL PRIMARY KEY,
-    password VARCHAR(150) NOT NULL,
-    role VARCHAR(30) DEFAULT 'User',
-    groupnames VARCHAR(200),
-    status VARCHAR(50) DEFAULT 'Pending'
-);
-
-CREATE TABLE user_group (
-    username VARCHAR(150) NOT NULL,
-    groupname VARCHAR(150) NOT NULL
-);
-
-CREATE TABLE upload (
-    username VARCHAR(150) NOT NULL,
-    filename VARCHAR(150) NOT NULL,
-    groupname VARCHAR(150) NOT NULL,
-    createdat DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedat DATETIME DEFAULT CURRENT_TIMESTAMP
+    createdate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedate DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO user VALUES ("Prashant", "Singh", "94.prashantsingh@gmail.com", "admin", "admin", "Admin", "No Group", "Approved");
